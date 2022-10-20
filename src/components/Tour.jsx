8fe25149c6f2dd520182b3503b9ab489c4a095b1
Tim <chibuiketimokonkwo@gmail.com>
2022-10-20 21:14:40 +0100

    updates

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -7,8 +7,11 @@ const Tour = ({id, name, image, info, price }) => {
             <footer>
                 <div className="tour-info">
                     <h4>{name}</h4>
-                    <h4 className="tour-price">{price}</h4>
+                    <h4 className="tour-price">${price}</h4>
                 </div>
+                <p>
+                    {info}
+                </p>
             </footer>
         </article>
     )
